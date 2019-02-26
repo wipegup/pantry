c5ba07bfe58c80164e6a42511d98002130335839
wipegup <wipegup@gmail.com>
2019-02-26 09:25:31 -0700

    test it exists for pantry

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,5 +4,17 @@ require './lib/recipe'
 require './lib/pantry'
 
 class PantryTest < MiniTest::Test
-  
+  def setup
+    @cheese = Ingredient.new("Cheese", "C", 50)
+    @mac = Ingredient.new("Macaroni", "oz", 200)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
 end
