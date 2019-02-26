9af5ec1de0d19333b4f595b292ab75eb43c20dde
wipegup <wipegup@gmail.com>
2019-02-26 09:39:34 -0700

    Add test it exists for cook book

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -4,5 +4,23 @@ require './lib/recipe'
 require './lib/cook_book'
 
 class CookBookTest < MiniTest::Test
+  def setup
+    @cookbook = CookBook.new
 
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    @ground_beef = Ingredient.new("Ground Beef", "oz", 100)
+    @bun = Ingredient.new("Bun", "g", 1)
+    @burger = Recipe.new("Burger")
+    @burger.add_ingredient(@ground_beef, 4)
+    @burger.add_ingredient(@bun, 100)
+  end
+
+  def test_it_exists
+    assert_instance_of CookBook, @CookBook
+  end
 end
