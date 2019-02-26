63955e7fe0b5dcdbcf66185766e1dbb3c2f892ec
wipegup <wipegup@gmail.com>
2019-02-26 09:11:35 -0700

    test it exists recipe

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -3,5 +3,13 @@ require './lib/recipe'
 require './lib/ingredient'
 
 class RecipeTest <MiniTest::Test
-  
+  def setup
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_exists
+    assert_instance_of Recipe, @mac_and_cheese
+  end
 end
