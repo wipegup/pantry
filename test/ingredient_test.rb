7e0b7d2a65d55a33252f5b4725e8966a6baea5bb
wipegup <wipegup@gmail.com>
2019-02-26 09:06:29 -0700

    Test it exists with setup for ingredient

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -2,4 +2,11 @@ require 'minitest/autorun'
 
 class IngredientTest < MiniTest::Test
 
+  def setup
+    @cheese = Ingredient.new("Cheese", "oz", 50)
+  end
+
+  def test_it_exists
+    assert_instance_of Ingredient, @cheese
+  end
 end
