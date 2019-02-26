ecceb6d6168cfa2ab0caf2b4e8f37c76cc182b92
wipegup <wipegup@gmail.com>
2019-02-26 09:49:39 -0700

    Skeleton for summary

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -6,7 +6,16 @@ class CookBook
   def initialize
     @recipes = []
   end
+
+  def add_recipe(recipe)
+    @recipes << recipe
+  end
+
   def summary
-    return @recipes
+    return @recipes.map{|recipe| recipe_summary(recipe)}
+  end
+
+  def recipe_summary(recipe)
+
   end
 end
