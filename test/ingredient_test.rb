306d830da0a5d1bbf52857e9d9551d005fe99883
wipegup <wipegup@gmail.com>
2019-02-26 09:08:22 -0700

    test for attributes in ingredients

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -10,4 +10,10 @@ class IngredientTest < MiniTest::Test
   def test_it_exists
     assert_instance_of Ingredient, @cheese
   end
+
+  def test_it_has_attributes
+    assert_equal "Cheese", @cheese.name
+    assert_equal "oz", @cheese.unit
+    assert_equal 50, @cheese.calories
+  end
 end
